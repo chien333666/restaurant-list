d88c481b5a420851aea61acff4915fe88df76bfa
chien333666 <chien333@gmail.com>
2024-03-12 23:45:52 +0800

    feat: add search funtion to listing

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
+const restaurants = require('./public/jsons/restaurant.json').results
 
 app.engine('.hbs', engine({ extname: '.hbs' }))
 app.set('view engine', '.hbs')
@@ -9,16 +10,29 @@ app.set('views', './views')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.redirect('/movies')
+  res.redirect('/restaurants')
 })
 
-app.get('/movies', (req, res) => {
-  res.render('index')
+app.get('/restaurants', (req, res) => {
+  // ?. 是一個選擇性鏈操作符，它允許在 req.query.search 為 undefined 或 null 時不會拋出錯誤，而是返回 undefined。
+  const keyword = req.query.search?.trim()
+  // 這行檢查 keyword 是否存在。如果存在，則進行篩選操作；如果不存在，則直接返回所有電影。
+  const matchedRestaurants = keyword ? restaurants.filter((mv) =>
+    Object.values(mv).some((property) => {
+      if (typeof property === 'string') {
+        return property.toLowerCase().includes(keyword.toLowerCase())
+      }
+      return false
+    })
+  ) : restaurants
+  // 當你將 restaurants: matchedRestaurants 作為參數傳遞給 res.render 方法時，你實際上是在告訴 Express 框架：“在渲染模板時，請將 matchedRestaurants 陣列作為 restaurants 變數傳遞給模板。” 這樣，在模板中，你可以使用 {{#each restaurants}} 和 {{/each}} 標籤來遍歷這個陣列，並使用 {{this}} 或 {{.}} 來訪問每個電影物件的屬性。
+  res.render('index', { restaurants: matchedRestaurants, keyword })
 })
 
-app.get('/movie/:id', (req, res) => {
+app.get('/restaurant/:id', (req, res) => {
   const id = req.params.id
-  res.send(`read movie: ${id}`)
+  const restaurant = restaurants.find((mv) => mv.id.toString() === id)
+  res.render('detail', { restaurant })
 })
 
 app.listen(port, () => {
